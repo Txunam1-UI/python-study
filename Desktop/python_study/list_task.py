6bb1f17dcbe025e16b4ee081f9c1bb533bdc6d14
Txunam1-UI <kalasingayvonne@gmail.com>
2026-09-21 19:38:46 +0300

    add new file

---
+++ Desktop/python_study/list_task.py
@@ -0,0 +1,29 @@
+
+#1. Display 2 from the list.
+trainees= ["John", [2, ["James","Mary"]]]
+print(trainees[1][0])
+
+#Output James  from the list.
+trainees= ["John", [2, ["James","Mary"]]]
+print(trainees[1][1][0])
+
+# Using a method add 56 at the end of the list.
+trainees.append('56')
+print(trainees)
+
+# Using a method add the name Mike between James and Mary
+trainees[1][1].insert(1,'Mike')
+print(trainees)
+
+#Change the value of 2 to 8
+trainees[1][0]='8'
+print(trainees)
+
+#Remove John and Mary from the list.
+trainees[1][1].remove('Mary')
+trainees.remove('John')
+print(trainees)
+
+
+# Using a function, determine the length of the list
+print(len(trainees))
